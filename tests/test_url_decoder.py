25e5d2bada66fe7f6624fc21fd520cc82d1b1baf
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2017-04-14 06:46:44 +0300

    Data Format 4 support (#7)
    * Updated url decoder to support weatherstation 2017-04-12
    
    * Added data formats to readme

--- tests/test_url_decoder.py
+++ tests/test_url_decoder.py
@@ -13,6 +13,7 @@ class TestUrlDecoder(TestCase):
         self.assertEqual(data['temperature'], 24)
         self.assertEqual(data['pressure'], 995)
         self.assertEqual(data['humidity'], 30)
+        self.assertEqual(data['identifier'], None)
 
     def test_decode_is_valid_case2(self):
         decoder = UrlDecoder()
@@ -21,3 +22,13 @@ class TestUrlDecoder(TestCase):
         self.assertEqual(data['temperature'], 27)
         self.assertEqual(data['pressure'], 995)
         self.assertEqual(data['humidity'], 28)
+        self.assertEqual(data['identifier'], None)
+
+    def test_decode_is_valid_weatherstation_2017_04_12(self):
+        decoder = UrlDecoder()
+        data = decoder.decode_data('AjUX1MAw0')
+
+        self.assertEqual(data['temperature'], 25.12)
+        self.assertEqual(data['pressure'], 992.0)
+        self.assertEqual(data['humidity'], 26.5)
+        self.assertEqual(data['identifier'], '0')
