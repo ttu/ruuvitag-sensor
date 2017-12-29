1a7e82756b8da7d7b3deedcc5e936ebd5ea0bc37
Dan Stahr <Dan.Stahr@gmail.com>
2017-12-29 07:39:47 +0000

    Merge Code Review: Added platform_system suffix

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='ruuvitag_sensor',
           'rx',
           'psutil;platform_system=="Linux"',
           'futures;python_version<"3.3"',
-          'ptyprocess'
+          'ptyprocess;platform_system=="Linux"'
       ],
       license='MIT',
       packages=['ruuvitag_sensor'],
