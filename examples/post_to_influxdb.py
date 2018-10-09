d71e1c43b6667eb07422de0add277159081f47cf
Otso Jousimaa <otso@ojousima.net>
2018-10-09 19:13:07 +0300

    Use mV with df5. Add data_format info to data. Update InfluxDB examples (#55)

--- examples/post_to_influxdb.py
+++ examples/post_to_influxdb.py
@@ -33,20 +33,37 @@ from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
 
 def convert_to_influx(mac, payload):
+    '''
+    Convert data into RuuviCollecor naming schme and scale
+
+    returns:
+        Object to be written to InfluxDB
+    '''
+    dataFormat = payload["data_format"] if ('data_format' in payload) else None
+    fields = {}
+    fields["temperature"]               = payload["temperature"] if ('temperature' in payload) else None
+    fields["humidity"]                  = payload["humidity"] if ('humidity' in payload) else None
+    fields["pressure"]                  = payload["pressure"] if ('pressure' in payload) else None
+    fields["accelerationX"]             = payload["acceleration_x"] if ('acceleration_x' in payload) else None
+    fields["accelerationY"]             = payload["acceleration_y"] if ('acceleration_y' in payload) else None
+    fields["accelerationZ"]             = payload["acceleration_z"] if ('acceleration_z' in payload) else None
+    fields["batteryVoltage"]            = payload["battery"]/1000.0 if hasattr(payload, 'battery') else None
+    fields["txPower"]                   = payload["tx_power"] if ('tx_power' in payload) else None
+    fields["movementCounter"]           = payload["battery"] if ('battery' in payload) else None
+    fields["measurementSequenceNumber"] = payload["measurement_sequence_number"] if ('measurement_sequence_number' in payload) else None
+    fields["tagID"]                     = payload["tagID"] if ('tagID' in payload) else None
+    fields["rssi"]                      = received_data[1]["rssi"] if ('rssi' in received_data[1]) else None
     return {
-        "measurement": "ruuvitag",
+        "measurement": "ruuvi_measurements",
         "tags": {
-            "mac": mac
+            "mac": mac,
+            "dataFormat": dataFormat
         },
-        "fields": {
-            "temperature": payload["temperature"],
-            "humidity": payload["humidity"],
-            "pressure": payload["pressure"]
-        }
+        "fields": fields
     }
 
 
-client = InfluxDBClient(host="localhost", port=8086, database="tag_data")
+client = InfluxDBClient(host="localhost", port=8086, database="ruuvi")
 
 while True:
     datas = RuuviTagSensor.get_data_for_sensors()
--- examples/post_to_influxdb_rx.py
+++ examples/post_to_influxdb_rx.py
@@ -7,24 +7,37 @@ Check guide and requirements from post_to_influxdb.py
 from influxdb import InfluxDBClient
 from ruuvitag_sensor.ruuvi_rx import RuuviTagReactive
 
-client = InfluxDBClient(host="localhost", port=8086, database="tag_data")
+client = InfluxDBClient(host="localhost", port=8086, database="ruuvitag")
 
 
 def write_to_influxdb(received_data):
+    '''
+    Convert data into RuuviCollecor naming schme and scale
+    '''
+    dataFormat = received_data[1]["data_format"] if ('data_format' in received_data[1]) else None
+    fields = {}
+    fields["temperature"]               = received_data[1]["temperature"] if ('temperature' in received_data[1]) else None
+    fields["humidity"]                  = received_data[1]["humidity"] if ('humidity' in received_data[1]) else None
+    fields["pressure"]                  = received_data[1]["pressure"] if ('pressure' in received_data[1]) else None
+    fields["accelerationX"]             = received_data[1]["acceleration_x"] if ('acceleration_x' in received_data[1]) else None
+    fields["accelerationY"]             = received_data[1]["acceleration_y"] if ('acceleration_y' in received_data[1]) else None
+    fields["accelerationZ"]             = received_data[1]["acceleration_z"] if ('acceleration_z' in received_data[1]) else None
+    fields["batteryVoltage"]            = received_data[1]["battery"]/1000.0 if hasattr(received_data[1], 'battery') else None
+    fields["txPower"]                   = received_data[1]["tx_power"] if ('tx_power' in received_data[1]) else None
+    fields["movementCounter"]           = received_data[1]["battery"] if ('battery' in received_data[1]) else None
+    fields["measurementSequenceNumber"] = received_data[1]["measurement_sequence_number"] if ('measurement_sequence_number' in received_data[1]) else None
+    fields["tagID"]                     = received_data[1]["tagID"] if ('tagID' in received_data[1]) else None
+    fields["rssi"]                      = received_data[1]["rssi"] if ('rssi' in received_data[1]) else None
     json_body = [
         {
-            "measurement": "ruuvitag",
+            "measurement": "ruuvi_measurements",
             "tags": {
-                "mac": received_data[0]
+                "mac": received_data[0],
+                "dataFormat": dataFormat
             },
-            "fields": {
-                "temperature": received_data[1]["temperature"],
-                "humidity": received_data[1]["humidity"],
-                "pressure": received_data[1]["pressure"]
-            }
+            "fields": fields
         }
     ]
-
     client.write_points(json_body)
 
 
