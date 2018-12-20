4d5395cd1d1f72690a0d9bf23664df575041fb94
Otso Jousimaa <otso@ojousima.net>
2018-12-20 18:20:14 +0200

    Fix batteryVoltage on Influx example. Use 'ruuvi' as database name (#63)

--- examples/post_to_influxdb.py
+++ examples/post_to_influxdb.py
@@ -47,9 +47,9 @@ def convert_to_influx(mac, payload):
     fields["accelerationX"]             = payload["acceleration_x"] if ('acceleration_x' in payload) else None
     fields["accelerationY"]             = payload["acceleration_y"] if ('acceleration_y' in payload) else None
     fields["accelerationZ"]             = payload["acceleration_z"] if ('acceleration_z' in payload) else None
-    fields["batteryVoltage"]            = payload["battery"]/1000.0 if hasattr(payload, 'battery') else None
+    fields["batteryVoltage"]            = payload["battery"]/1000.0 if ('battery' in payload) else None
     fields["txPower"]                   = payload["tx_power"] if ('tx_power' in payload) else None
-    fields["movementCounter"]           = payload["battery"] if ('battery' in payload) else None
+    fields["movementCounter"]           = payload["movement_counter"] if ('movement_counter' in payload) else None
     fields["measurementSequenceNumber"] = payload["measurement_sequence_number"] if ('measurement_sequence_number' in payload) else None
     fields["tagID"]                     = payload["tagID"] if ('tagID' in payload) else None
     fields["rssi"]                      = payload["rssi"] if ('rssi' in payload) else None
