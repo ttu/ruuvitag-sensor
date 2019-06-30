adadd50a8fe07e3c53cd46bee84e543f631cabfa
ttu <tomi.tuhkanen@iki.fi>
2019-06-30 08:52:46 +0300

    Option to use package from pypi in verification

--- verification.sh
+++ verification.sh
@@ -5,26 +5,37 @@ rm -rf venv_py2
 
 python3 -m venv venv_py3
 source venv_py3/bin/activate
-pip3 install -e .
+if [ "$1" = "pypi" ]; then
+  python3 -m pip install ruuvitag_sensor
+else
+  python3 -m pip install -e .
+fi
 python3 verification.py
 ret=$?
 if [ $ret -ne 0 ]; then
-     echo 'Python 3 verification failed'
-     return 1
+  echo 'Python 3 verification failed'
+  rm -rf venv_py3
+  exit 1
 fi
 deactivate
+rm -rf venv_py3
 
 virtualenv --python=/usr/bin/python2.7 venv_py2
 source venv_py2/bin/activate
-pip install -e .
+if [ "$1" = "pypi" ]; then
+  pip install ruuvitag_sensor
+else
+  pip install -e .
+fi
 python verification.py
 ret=$?
 if [ $ret -ne 0 ]; then
-     echo 'Python 2.7 verification failed'
-     return 1
+  echo 'Python 2.7 verification failed'
+  rm -rf venv_py2
+  exit 1
 fi
 deactivate
-
+rm -rf venv_py2
 
 echo '----------------------'
 echo 'VERIFICATION COMPLETED'
