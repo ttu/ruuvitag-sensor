5e9131bd897b6ea32909e32382bce5c175a75049
ttu <tomi.tuhkanen@iki.fi>
2020-03-15 08:42:20 +0200

    Use markdown as long description content type

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
+import io
 from setuptools import setup
 
 import ruuvitag_sensor
 
-try:
-    import pypandoc
-    readme = pypandoc.convert_file('README.md', 'rst')
-    readme = readme.replace('\r', '')
-except ImportError:
-    import io
-    with io.open('README.md', encoding='utf-8') as f:
-        readme = f.read()
+
+with io.open('README.md', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(name='ruuvitag_sensor',
       version=ruuvitag_sensor.__version__,
       description='Find RuuviTag sensor beacons, get and encode data from ' +
                   'selected sensors',
       long_description=readme,
+      long_description_content_type="text/markdown",
       url='https://github.com/ttu/ruuvitag-sensor',
       download_url='https://github.com/ttu/ruuvitag-sensor/tarball/' +
                    ruuvitag_sensor.__version__,
