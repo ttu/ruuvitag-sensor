ff4d0363ecca1411b61e6637d007d7b1a51d34ad
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-06-19 22:10:29 +0300

    Fix handling of ruuvit manufacturer_data

--- examples/get_async_bleak.py
+++ examples/get_async_bleak.py
@@ -1,18 +1,15 @@
 import asyncio
-import logging
-
 import os
-from ruuvitag_sensor.log import enable_console
 os.environ['RUUVI_BLE_ADAPTER'] = 'Bleak'
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+import ruuvitag_sensor.log
 
-enable_console()
+ruuvitag_sensor.log.enable_console()
 
-log = logging.getLogger(__name__)
 
 def handle_data(found_data):
-    log.info(found_data)
+    print(found_data)
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(RuuviTagSensor.get_datas_async(handle_data))
--- ruuvitag_sensor/adapters/bleak_ble.py
+++ ruuvitag_sensor/adapters/bleak_ble.py
@@ -15,21 +15,36 @@ class BleCommunicationBleak(BleCommunicationAsync):
 
     @staticmethod
     async def _start(queue: asyncio.Queue, blacklist: list[str]):
-        '''
-        Attributes:
-            device (string): BLE device (default 0)
-        '''
 
         async def detection_callback(device: BLEDevice, advertisement_data: AdvertisementData):
             mac = device.address
             if mac and mac in blacklist:
                 log.debug('MAC blacklised: %s', mac)
                 return
-            # TODO: Convert data to a format similar to the rest ruuvi pipeline
-            log.info(advertisement_data)
-            manufacturer_data = advertisement_data.manufacturer_data[76]
-            hex = manufacturer_data.hex()
-            await queue.put((mac, hex))
+
+            # Do all RuuviTag's have data in 1177?
+            if not 1177 in advertisement_data.manufacturer_data:
+                return
+
+            data = advertisement_data.manufacturer_data[1177]
+            # Bleak returns data in a different format than the nix_hci
+            # adapter. Since the rest of the processing pipeline is
+            # somewhat reliant on the additional data, add to the
+            # beginning of the actual data:
+            #
+            # - An FF type marker with 9904 (Ruuvi manufacturer identifer)
+            # - A length marker, covering the vendor specific data
+            # - Another length marker, covering the length-marked
+            #   vendor data.
+            #
+            # Thus extended, the result can be parsed by the rest of
+            # the pipeline.
+            #
+            # TODO: This is kinda awkward, and should be handled better.
+            data = 'FF9904' + data.hex()
+            data = '%02x%s' % (len(data) >> 1, data)
+            data = '%02x%s' % (len(data) >> 1, data)
+            await queue.put((mac, data))
 
         scanner.register_detection_callback(detection_callback)
         await scanner.start()
@@ -39,15 +54,13 @@ class BleCommunicationBleak(BleCommunicationAsync):
         scanner.stop()
 
     @staticmethod
-    async def get_datas(blacklist: list[str] = [], bt_device: str = ''):
+    async def get_datas(blacklist: list[str] = [], bt_device: str = '') -> tuple[str, str]:
         await BleCommunicationBleak._start(q, blacklist)
 
         try:
             while True:
                 next_item = await q.get()
-                mac = next_item[0]
-                manufacturer_data = next_item[1]
-                yield (mac, manufacturer_data)
+                yield next_item
         except KeyboardInterrupt as ex:
             pass
         except Exception as ex:
