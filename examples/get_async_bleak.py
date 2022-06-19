ffe82f199de0c6ae64c156ed2c7028b9c4c37adc
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-06-19 08:31:54 +0300

    Enable console log for example

--- examples/get_async_bleak.py
+++ examples/get_async_bleak.py
@@ -1,15 +1,18 @@
 import asyncio
+import logging
 
 import os
+from ruuvitag_sensor.log import enable_console
 os.environ['RUUVI_BLE_ADAPTER'] = 'Bleak'
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
+enable_console()
+
+log = logging.getLogger(__name__)
 
 def handle_data(found_data):
-    print(found_data)
-    # print('MAC ' + found_data[0])
-    # print(found_data[1])
+    log.info(found_data)
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(RuuviTagSensor.get_datas_async(handle_data))
