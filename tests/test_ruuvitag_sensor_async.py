9f252fd63e28e1ad2bb91723d906b4bd7dae73b3
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-06-20 23:40:49 +0300

    Add note on skipping async tests on CI

--- tests/test_ruuvitag_sensor_async.py
+++ tests/test_ruuvitag_sensor_async.py
@@ -3,14 +3,25 @@ from unittest.mock import patch
 import pytest
 import os
 os.environ['RUUVI_BLE_ADAPTER'] = 'bleak'
+"""
+NOTE: Execute tests manually. These are not part of CI
+Setting env variables for CI or READTHEDOCS didn't work with Travis
+
+# https://github.com/hbldh/bleak/discussions/475
+
+.tox/py37-basic_linux/lib/python3.7/site-packages/bleak/__init__.py:41: in <module>
+    if not _on_ci and not check_bluez_version(5, 43):
+.tox/py37-basic_linux/lib/python3.7/site-packages/bleak/backends/bluezdbus/__init__.py:17: in check_bluez_version
+    p = subprocess.Popen(["bluetoothctl", "--version"], stdout=subprocess.PIPE)
+
+FileNotFoundError: [Errno 2] No such file or directory: 'bluetoothctl': 'bluetoothctl'
+"""
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
 # pylint: disable=line-too-long,no-self-use,unused-argument
 
-# pytest
-# pytest-asyncio
-
+@pytest.mark.skip(reason="Doesn't work with CI")
 class TestRuuviTagSensorAsync:
 
     async def _get_datas(self, blacklist=[], bt_device='') -> Tuple[str, str]:
