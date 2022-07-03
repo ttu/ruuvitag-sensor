65715d61325b2a24051bc0c55c0aae6a1043bf99
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-07-03 14:32:41 +0300

    Types to BleCommunication base class

--- ruuvitag_sensor/adapters/__init__.py
+++ ruuvitag_sensor/adapters/__init__.py
@@ -1,16 +1,18 @@
 import abc
+from typing import Iterator, List, Tuple
 
 
 class BleCommunication(object):
     """Bluetooth LE communication"""
+
     __metaclass__ = abc.ABCMeta
 
     @staticmethod
     @abc.abstractmethod
-    def get_first_data(mac, bt_device=''):
+    def get_first_data(mac: str, bt_device: str = "") -> str:
         pass
 
     @staticmethod
     @abc.abstractmethod
-    def get_datas(blacklist=[], bt_device=''):
+    def get_datas(blacklist: List(str) = [], bt_device: str = "") -> Iterator[Tuple[str, str]]:
         pass
