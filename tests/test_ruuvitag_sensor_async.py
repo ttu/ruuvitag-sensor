524791600d19ad9ee9c1021062d4719731808c47
ttu <tomi.tuhkanen@iki.fi>
2022-08-03 23:02:58 +0300

    Rename variables from tests

--- tests/test_ruuvitag_sensor_async.py
+++ tests/test_ruuvitag_sensor_async.py
@@ -25,32 +25,32 @@ from ruuvitag_sensor.ruuvi import RuuviTagSensor
 class TestRuuviTagSensorAsync:
 
     async def _get_data(self, blacklist=[], bt_device='') -> Tuple[str, str]:
-        datas = [
+        tag_data = [
             ('EB:A5:D1:02:CE:68', '1c1bFF99040513844533c43dffe0ffd804189ff645fcffeba5d102ce68'),
             ('CD:D4:FA:52:7A:F2', '1c1bFF990405128a423bc45fffd8ff98040cafd6497a83cdd4fa527af2'),
             ('EC:4D:A7:95:08:6B', '1c1bFF9904050d3f5306c4df0024ffe404108f562787f1ec4da795086b')
         ]
 
-        for data in datas:
+        for data in tag_data:
             yield data
 
     @patch('ruuvitag_sensor.adapters.bleak_ble.BleCommunicationBleak.get_data', _get_data)
     @pytest.mark.asyncio
     async def test_get_data_async(self):
-        datas = []
+        data = []
         gener = RuuviTagSensor.get_data_async()
-        async for data in gener:
-            datas.append(data)
+        async for received in gener:
+            data.append(received)
 
-        assert len(datas) == 3
+        assert len(data) == 3
 
     @patch('ruuvitag_sensor.adapters.bleak_ble.BleCommunicationBleak.get_data', _get_data)
     @pytest.mark.asyncio
     async def test_get_data_async_with_macs(self):
-        datas = []
+        data = []
         macs = ['EB:A5:D1:02:CE:68', 'EC:4D:A7:95:08:6B']
         gener = RuuviTagSensor.get_data_async(macs)
-        async for data in gener:
-            datas.append(data)
+        async for received in gener:
+            data.append(received)
 
-        assert len(datas) == 2
+        assert len(data) == 2
