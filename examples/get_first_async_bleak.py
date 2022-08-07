60c434590d0d5ecafdba81503db854953621a4a0
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-08-07 09:14:46 +0300

    Update get first raw example

--- examples/get_first_async_bleak.py
+++ examples/get_first_async_bleak.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from ruuvitag_sensor.decoder import Df5Decoder
 os.environ['RUUVI_BLE_ADAPTER'] = 'bleak'
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
@@ -9,8 +10,16 @@ ruuvitag_sensor.log.enable_console()
 
 
 async def main():
-    data = await RuuviTagSensor.get_first_raw_data_async('FE:52:F7:B3:65:CC')
-    print(data)
+    """
+    NOTE: This is for example use of get_first_raw_data_async. Normally use get_data_async-method.
+
+    async for data in RuuviTagSensor.get_data_async(['FA:52:F7:B3:65:CC']):
+        print(data)
+    """
+    data = await RuuviTagSensor.get_first_raw_data_async('FA:52:F7:B3:65:CC')
+    print(f"Raw: {data}")
+    decoded = Df5Decoder().decode_data(data[1])
+    print(f"Decoded: {decoded}")
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
