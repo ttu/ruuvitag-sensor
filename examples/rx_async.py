5bbaf860ac71dcaec77eb3ddfe4bbb5448fa5c6f
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2023-04-11 22:09:28 +0300

    Async rx example

---
+++ examples/rx_async.py
@@ -0,0 +1,19 @@
+import os
+import asyncio
+
+os.environ["RUUVI_BLE_ADAPTER"] = "bleak"
+
+from ruuvitag_sensor.ruuvi_rx import RuuviTagReactive
+
+
+async def main():
+    ruuvi_rx = RuuviTagReactive()
+    subject = ruuvi_rx.get_subject()
+    subject.subscribe(print)
+
+
+if __name__ == "__main__":
+    # https://stackoverflow.com/a/56727859/1292530
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.run_forever()
