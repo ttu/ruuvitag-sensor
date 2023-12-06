147b6f12f2a4130a9b5d2b95c5ddade468ac23fd
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2023-12-06 07:50:05 +0200

    refactor: debug log bleak received data (#234)

--- ruuvitag_sensor/adapters/bleak_ble.py
+++ ruuvitag_sensor/adapters/bleak_ble.py
@@ -72,6 +72,8 @@ class BleCommunicationBleak(BleCommunicationAsync):
             if 1177 not in advertisement_data.manufacturer_data:
                 return
 
+            log.debug("Received data: %s", advertisement_data)
+
             data = BleCommunicationBleak._parse_data(advertisement_data.manufacturer_data[1177])
 
             # Add RSSI to encoded data as hex. All adapters use a common decoder.
@@ -80,6 +82,8 @@ class BleCommunicationBleak(BleCommunicationAsync):
 
         scanner = _get_scanner(detection_callback)
         await scanner.start()
+
+        log.debug("Bleak scanner started")
 
         try:
             while True:
@@ -93,6 +97,8 @@ class BleCommunicationBleak(BleCommunicationAsync):
             log.info(ex)
 
         await scanner.stop()
+
+        log.debug("Bleak scanner stopped")
 
     @staticmethod
     async def get_first_data(mac: str, bt_device: str = "") -> RawData:
